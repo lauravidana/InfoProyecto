b6112f27a9fb6f7924be1bc2b390514733a2e1a8
Laura Vidaña <lauravidana@hotmail.com>
2021-12-21 20:50:24 -0500

    log in

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.posts'
-    'apps.user'
+    'apps.posts',
+    'apps.user',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(os.path.dirname(BASE_DIR),'static'),)
