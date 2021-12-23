fbce25300b22580685f20dace1bb50473b5a6cea
Laura Vidaña <lauravidana@hotmail.com>
2021-12-23 11:31:42 -0500

    comentarios agregados

--- blog/apps/posts/views.py
+++ blog/apps/posts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from .models import Post
+from .models import Post, Comentario
 from .forms import PostForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
@@ -18,6 +18,10 @@ class ListarPosts(LoginRequiredMixin, ListView):
 class DetallePost(LoginRequiredMixin, DetailView):
 	model = Post
 	template_name="posts/post_detail.html"
+	def get_context_data(self, **kwargs):
+		data = super().get_context_data(**kwargs)
+		data['comentarios'] = Comentario.objects.all()
+		return data 
 
 class CrearPost(CreateView):
 	model = Post
